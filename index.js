197dc2ecb7b6692d43c1c869e3f88623ee06d8a9
chandreshma <chandreshma02@gmail.com>
2022-11-24 12:29:26 +0530

    added about post action

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const port=4000
+const bodyParser=require("body-parser")
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("hello")
+})
+app.post("/newData",(req,res)=>{
+	const{name,password}=req.body
+	console.log(name,password)
+	res.send("Data Added")
+})
+
+app.listen(port,()=>console.log("Server has started"))
